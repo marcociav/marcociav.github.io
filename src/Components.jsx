3b36ac5a6ccaece568bdce0e00fa717b31410963
marcociav <marco.bizzness@gmail.com>
2022-05-21 00:38:13 +0200

    First commit

---
+++ src/Components.jsx
@@ -0,0 +1,17 @@
+import { useState} from 'react';
+import logo from './logo.png';
+import './Components.css';
+
+
+export function Portfolio() {
+
+    return (
+        <div className="container">
+            <div className="banner-container">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+            </div>
+        </div>
+    )
+}
