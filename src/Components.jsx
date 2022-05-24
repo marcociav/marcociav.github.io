56a0b64a35e75a34cc4bc7d2411d6dd133f2594a
marcociav <marco.bizzness@gmail.com>
2022-05-24 19:42:30 +0200

    First working version up

--- src/Components.jsx
+++ src/Components.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.png';
 import './Components.css';
+import { AiOutlineGithub, AiOutlineLinkedin} from 'react-icons/ai'
 
 
 export function Portfolio() {
@@ -7,7 +8,7 @@ export function Portfolio() {
     return (
         <div className="container">
             <Banner/>
-                <div className="content-container">   
+            <div className="content-container">   
                 <Section 
                     id="tech-products-container"
                     title="Tech Products"
@@ -27,8 +28,13 @@ export function Portfolio() {
                     />
                     <SubSection
                         title={`Smile App [ WIP ]`}
-                        description={`A web app that puts a smile on your face using a CycleGAN.`}
-                    />
+                        description={`A web app that puts a smile on your face using a CycleGAN.\nAPI in development:`}
+                    >
+                        <TextLink
+                            text="https://smile-app-api-e6l5ytsm6a-pd.a.run.app/docs"
+                            link="https://smile-app-api-e6l5ytsm6a-pd.a.run.app/docs"
+                        />
+                    </SubSection>
                 </Section>
                 <Section
                     id="accomplishments-container"
@@ -44,22 +50,24 @@ export function Portfolio() {
                     <SubSection
                         title="Publications"
                     >
-                        <SubSection
+                        <SubList
                             title="On the training of sparse and dense deep neural networks: less parameters, same performance"
                             description="Training deep neural networks in reciprocal space."
                             link="https://arxiv.org/abs/2106.09021"
                         />
                     </SubSection>
                 </Section>
+                <Footer/>
             </div>
         </div>
     )
 }
 
+
 function Banner() {
 
     return (
-        <div className="banner-container">
+        <header className="banner-container">
             <div className="name-container">
                 <img src={logo} className="app-logo" alt="logo" />
                 <h1 className="name">Marco Ciavarella</h1>
@@ -69,7 +77,7 @@ function Banner() {
                     Data Scientist, Machine Learning Engineer, Physicist, Human
                 </p>
             </div>
-        </div>
+        </header>
     )
 }
 
@@ -86,13 +94,39 @@ function Section(props) {
 }
 
 
+function TextLink(props) {
+    var textLinkComponent;
+    if (props.link) {
+        textLinkComponent = 
+        <a 
+            href={props.link} 
+            target="_blank"
+            onMouseEnter={(e) => {
+                e.target.style.color = "aquamarine"
+            }}
+            onMouseLeave={(e) => {
+                e.target.style.color = "white"
+            }}
+        >
+            {props.text}
+        </a>;
+    } else {
+        textLinkComponent = <a>{props.text}</a>;
+    }    
+    return (textLinkComponent);
+}
+
+
 function SubSection(props) {
     return (
         <div 
             className="subsection-container"
         >
             <h3>
-                <a href={props.link} target="_blank">{props.title}</a>
+                <TextLink
+                    text={props.title}
+                    link={props.link}
+                />
             </h3>
             <p style={{whiteSpace: "pre-line", marginTop: "3px"}}>
                 {props.description}
@@ -101,3 +135,49 @@ function SubSection(props) {
         </div>
     )
 }
+
+
+function SubList(props) {
+    return (
+        <div className="sublist-container">
+            <h4>
+                <TextLink
+                    text={props.title}
+                    link={props.link}
+                />
+            </h4>
+            <p style={{whiteSpace: "pre-line", marginTop: "3px"}}>
+                {props.description}
+            </p>
+        </div>
+    )
+}
+
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <AiOutlineGithub
+                style={{fontSize: "20pt", cursor: "pointer", fill: "white"}}
+                onClick={() => window.open("https://github.com/marcociav/", "_blank")}
+                onMouseEnter={(e) => {
+                    e.target.style.fill = "aquamarine"
+                }}
+                onMouseLeave={(e) => {
+                    e.target.style.fill = "white"
+                }}
+            />
+            <AiOutlineLinkedin
+                style={{fontSize: "20pt", cursor: "pointer", fill: "white"}}
+                onClick={() => window.open("https://www.linkedin.com/in/marco-ciavarella-mrccv/?locale=en_US", "_blank")}
+                onMouseEnter={(e) => {
+                    e.target.style.fill = "aquamarine"
+                }}
+                onMouseLeave={(e) => {
+                    e.target.style.fill = "white"
+                }}
+             />
+        </div>
+    )
+}
+
